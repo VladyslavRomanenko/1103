d76e6c7194be443d123a873bf06b672b0b4b750f
Vladyslav <vladyslavromanenkoo@gmail.com>
2024-02-27 19:25:53 +0200

    change video

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,11 @@
-// import css from "./Hero.module.css";
-// import photo from "../../assets/images/wallet/IMG_5096.jpg";
 import video from "../../assets/video/video.mp4";
 
 const Hero = () => {
   return (
     <div>
-      <video src={video} width="100%" playsInline autoPlay={true}></video>
+      <video width="100%" autoPlay loop muted>
+        <source src={video} />
+      </video>
     </div>
   );
 };
